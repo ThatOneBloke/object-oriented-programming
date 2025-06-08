e68645665f69b2ede79c0c06e188131a41c9b223
ThatOneBloke <x1648811@gmail.com>
2025-06-08 16:41:09 +0100

    Add files via upload

---
+++ growingcircle2.py
@@ -0,0 +1,38 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+
+class Circle():
+    def __init__ (self, radius, color, pos, width):
+        self.radius = radius
+        self.color = color
+        self.pos = pos
+        self.width = width
+        self.surface = screen
+    
+    def draw(self):
+        pygame.draw.circle(self.surface, self.color, self.pos, self.radius, self.width)
+    
+    def grow(self, R):
+        self.radius = self.radius + R
+        pygame.draw.circle(self.surface, self.color, self.pos, self.radius, self.width)
+
+bluecircle = Circle(10, "grey", (250, 250), 0)
+
+run = True
+while run:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            run = False
+        if i.type == pygame.MOUSEBUTTONDOWN:
+            screen.fill("black")
+            bluecircle.draw()
+            pygame.display.update()
+        if i.type == pygame.MOUSEBUTTONUP:
+            screen.fill("black")
+            bluecircle.grow(10)
+        if i.type == pygame.MOUSEMOTION:
+            pos = pygame.mouse.get_pos()
+            blackcircle = Circle(5, "white", pos, 0)
+            blackcircle.draw()
+            pygame.display.update()
